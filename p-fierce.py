f79f423659f70e2b10ccd9db4022f915bffb638e
Odd Meta <odd.meta@gmail.com>
2013-01-18 05:34:06 -0500

    Added usage options, followed some silly PEP8 recommendations

--- p-fierce.py
+++ p-fierce.py
@@ -11,14 +11,12 @@ import getopt
 
 #gank ARIN results formatting code out of fierce2 (or find something that does the same thing) and port to python for automating range discovery
     #write whois range discovery bits
-    
+
 #add option to run a command on hosts after sorting them out
 
 #add option to output all hosts to a file in various formats (just IPs, hostnames and IPs)
 
 #give option to filter hosts based on a common list of critical words (access, vpn, mail, employee, portal, cms, etc)
-
-
 
 
 #COMMAND LINE OPTION DEFAULTS
@@ -31,13 +29,14 @@ def usage():
     #thanks to http://www.network-science.de/ascii/ for the ascii art
     print "              __ _                   \n             / _(_)                  \n _ __ ______| |_ _  ___ _ __ ___ ___ \n| '_ \______|  _| |/ _ \ '__/ __/ _ \\\n| |_) |     | | | |  __/ | | (_|  __/\n| .__/      |_| |_|\___|_|  \___\___|\n| |                                  \n|_| \n"
     print "p-fierce v0.1"
+    print "usage: p-fierce [options] filename"
     print "takes a fierce2 xml or plain text file(s) (eventually)\nand processes out ranges of IPs, groups ips in lists, etc"
     print "(currently only does Prefix Bruteforce section copy-pasted into a new file)"
     print "written by:\n@odd_meta - odd.meta@gmail.com"
     print "\nreleased under the 'you'll just steal it for work anyway'(tm) license 2012/12/18"
 
     print """
-    
+
     Usage:
         -v: be more verbose
         -h: display this screen and exit
@@ -46,7 +45,7 @@ def usage():
             1 = 10.0.0.0/8
             2 = 10.0.0.0/16
             3 = 10.0.0.0/24
-	example:
+    example:
         ./p-fierce.py -n 1 fierce.output.txt
     """
 
@@ -55,7 +54,7 @@ try:
     opts, args = getopt.getopt(sys.argv[1:], "hvn:", ["help"])
 except getopt.GetoptError as err:
     # print help information and exit:
-    print str(err) # will print something like "option -a not recognized"
+    print str(err)  # will print something like "option -a not recognized"
     usage()
     sys.exit(2)
 OUTPUT = None
@@ -65,7 +64,7 @@ DISPLAY_SPLIT = 2
 if len(args) == 0:
     usage()
     sys.exit(2)
-    
+
 fierce_files = []
 
 fierce_files = args[:]
@@ -74,7 +73,7 @@ for o, a in opts:
     if o == "-v":
         VERBOSE = True
     elif o == "-n":
-        DISPLAY_SPLIT = int( a[0] )
+        DISPLAY_SPLIT = int(a[0])
     elif o in ("-h", "--help"):
         usage()
         sys.exit()
@@ -88,11 +87,11 @@ def temp_process_fierce_files(raw_fierce_handles):
     for fhandle in raw_fierce_handles:
         for line in fhandle:
             split_line = line.split("\t")
-            
+
             clean_line = []
-            
+
             for item in split_line:
-                clean_line.append( item.strip() )
+                clean_line.append(item.strip())
             if len(clean_line) > 1:
                 fierce_hosts.append(clean_line)
 
@@ -111,7 +110,7 @@ def process_hosts(fierce_hosts):
     for host in fierce_hosts:
         ip = host[0]
         ip_split = ip.split(".")
-        
+
         if ip_split[0] == "10":
             internal_ips.append(host)
             continue
@@ -119,7 +118,7 @@ def process_hosts(fierce_hosts):
         if ip_split[0] == "192" and ip_split[1] == "168":
             internal_ips.append(host)
             continue
-            
+
         if ip_split[0] == "172" and ( ip_split[1] > 15 or ip_split[1] < 32 ):
             internal_ips.append(host)
             continue
